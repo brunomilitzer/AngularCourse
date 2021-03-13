9093a07a63d5a526468ef77a0e6ed8f913452b4d
Bruno Militzer <brunomilitzer.work@gmail.com>
2021-03-13 21:00:47 +0000

    Added Form functionalities

--- forms-td/src/app/app.component.ts
+++ forms-td/src/app/app.component.ts
@@ -8,9 +8,34 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName(): void {
     const suggestedName = 'Superuser';
+    /*this.signupForm.setValue({
+      userData: {
+        username: suggestedName,
+        email: ''
+      },
+      secret: 'pet',
+      questionAnswer: '',
+      gender: 'male'
+    });*/
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
 /*  onSubmit(form: NgForm): void {
@@ -18,6 +43,13 @@ export class AppComponent {
   }*/
 
   onSubmit(): void {
-    console.log(this.signupForm);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.userData.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.submitted = true;
+    this.signupForm.reset();
   }
 }
