897fd75119e8d5f243f9306f5e56ced3288a519f
Bruno Militzer <brunomilitzer.work@gmail.com>
2021-03-27 19:09:23 +0000

    Added Ngrx Dev Tools

--- recipe/src/app/app.module.ts
+++ recipe/src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core.module';
-import * as fromApp from './store/app.reducer';
-import { EffectsModule } from '@ngrx/effects';
+import { environment } from '../environments/environment';
 import { AuthEffects } from './auth/store/auth.effects';
+import * as fromApp from './store/app.reducer';
+import { CoreModule } from './core.module';
+import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule( {
   declarations: [
@@ -23,6 +26,8 @@ import { AuthEffects } from './auth/store/auth.effects';
     AppRoutingModule,
     StoreModule.forRoot( fromApp.appReducer ),
     EffectsModule.forRoot( [ AuthEffects ] ),
+    StoreDevtoolsModule.instrument( { logOnly: environment.production } ),
+    StoreRouterConnectingModule.forRoot(),
     SharedModule,
     CoreModule
   ],
