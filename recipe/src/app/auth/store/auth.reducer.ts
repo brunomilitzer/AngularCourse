5c9d15f245dbbde9656c1643b80a2c0d2d0640cb
Bruno Militzer <brunomilitzer.work@gmail.com>
2021-03-27 16:32:22 +0000

    Added NgRx Login Functionality

--- recipe/src/app/auth/store/auth.reducer.ts
+++ recipe/src/app/auth/store/auth.reducer.ts
@@ -1,20 +1,22 @@
 import { User } from '../user.model';
 import * as AuthActions from './auth.actions';
-import { LOGIN, LOGOUT } from './auth.actions';
 
 export interface State {
   user: User;
+  authError: string;
+  loading: boolean;
 }
 
 const initialState: State = {
-  user: null
+  user: null,
+  authError: null,
+  loading: false
 };
 
 export function authReducer( state = initialState, action: AuthActions.AuthActions ): State {
 
-  console.log( state );
   switch ( action.type ) {
-    case LOGIN:
+    case AuthActions.LOGIN:
       const user = new User(
         action.payload.email,
         action.payload.userId,
@@ -23,13 +25,29 @@ export function authReducer( state = initialState, action: AuthActions.AuthActio
       );
       return {
         ...state,
-        user
+        authError: null,
+        user,
+        loading: false
       };
-    case LOGOUT:
+    case AuthActions.LOGOUT:
       return {
         ...state,
         user: null
       };
+    case AuthActions.LOGIN_START:
+      return {
+        ...state,
+        authError: null,
+        loading: true
+      };
+    case AuthActions.LOGIN_FAIL:
+      return {
+        ...state,
+        user: null,
+        authError: action.payload,
+        loading: false
+      };
+
     default:
       return state;
   }
